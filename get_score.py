28b4d63a2517f298a329ec8dddfa9416d41e1dec
Aaron Davidson <aarond@alumni.cmu.edu>
2022-09-21 19:41:11 -0400

    Add test and extract modes Notes: e argument must be before t argument Don't run the 'run' script's install mode Instead, pip install -r requirements.txt

--- get_score.py
+++ get_score.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 # import the necessary packages
-from skimage.measure import compare_nrmse
+from skimage.metrics import normalized_root_mse as compare_nrmse
 from sys import argv, exit, stderr
 from os.path import dirname
 import cv2
 
 # get filenames
 fst_file = argv[1]
-if fst_file[-4:] != ".jpg":
+if fst_file[-4:] != '.jpg':
     exit()
 
 # Set current image and previous image
@@ -20,26 +20,21 @@ prev = curr - 1
 # 2nd image has no previous image (gives errors as prev is BG)
 if curr == 0:
     print("0 nan")
-    print("Background Image", file=stderr)
+    stderr.write("Background Image\n")
     exit()
 if curr == 1:
     print("1 nan")
-    print("First Image, no previous images", file=stderr)
+    stderr.write("First Image, no previous images\n")
     exit()
 
-snd_file = fst_file[:-10] + str(prev).rjust(6, '0') + fst_file[-4:]
+snd_file = fst_file[:-10] + str(prev).rjust(6, '0') + '.jpg'
 
 bg_file = fst_file[:-10] + '000000.jpg'
 
-# load the two input images and background image
-#imageA = cv2.imread(fst_file)
-#imageB = cv2.imread(snd_file)
-#imageBG = cv2.imread(bg_file)
-
 # convert the images to grayscale
-grayA = cv2.imread(fst_file,0)
-grayB = cv2.imread(snd_file,0)
-grayBG = cv2.imread(bg_file,0)
+grayA = cv2.imread(fst_file, 0)
+grayB = cv2.imread(snd_file, 0)
+grayBG = cv2.imread(bg_file, 0)
 
 # Remove background and threshold to remove shadow effects
 threshold = 20
@@ -60,21 +55,22 @@ deviance = 1.0
 scr_diff = 0.0
 dev_diff = 0.0
 if curr > 5:
-    trd_file = fst_file[:-10] + str(curr-5).rjust(6, '0') + fst_file[-4:]
-    #imageC = cv2.imread(trd_file)
-    grayC = cv2.imread(trd_file,0)
+    trd_file = fst_file[:-10] + str(curr - 5).rjust(6, '0') + '.jpg'
+    grayC = cv2.imread(trd_file, 0)
     diffC = cv2.absdiff(grayC, grayBG)
     thresC = cv2.threshold(diffC, threshold, 255, cv2.THRESH_BINARY)[1]
     deviance = compare_nrmse(thresA, thresC)
+    logfile = dirname(fst_file) + '/output.log'
 
     # Calculate difference compared with previous layer score and deviance
-    logfile = dirname(fst_file) + '/output.log'
-    with open(logfile) as log:
+    with open(logfile, 'r') as log:
         data = log.readlines()
     prev_layer = data[-1]
-    layer,scr,dev,s_diff,d_diff = prev_layer.split(" ")
+    layer, scr, dev, s_diff, d_diff = prev_layer.split(" ")
     scr_diff = abs(score-float(scr))
     dev_diff = abs(deviance-float(dev))
 
-print("{} {} {} {} {}".format(curr,score,deviance,scr_diff,dev_diff))
-print("Image: {:d}\t Score: {}\t Deviance: {}\tDiffs: {}/{}".format(curr,score,deviance,scr_diff,dev_diff), file=stderr)
+print("{} {} {} {} {}".format(curr, score, deviance, scr_diff, dev_diff))
+stderr.write("Image: {:d}\t Score: {}\t Deviance: {}\tDiffs: {}/{}\n".format(
+    curr, score, deviance, scr_diff, dev_diff
+))
